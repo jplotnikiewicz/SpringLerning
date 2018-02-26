84a1293aa644d8727ea5d3e8b80a2e9c02cf45a9
jplotnikieicz <https://dev.codedose.com/gitlab/>
2018-02-26 17:29:11 +0100

    Spring lerning

--- src/main/java/springFramework/core/Core.java
+++ src/main/java/springFramework/core/Core.java
@@ -1,11 +1,34 @@
 package springFramework.core;
 
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.GenericApplicationContext;
 
 public class Core {
 
+
+
+    public static void main (String [] args){
+        start2();
+
+    }
+
+    public static void start1(){
+        ApplicationContext context = new ClassPathXmlApplicationContext("Spring/services.xml");
+        PetStoreServiceImpl petStoreService = context.getBean("petStore",PetStoreServiceImpl.class);
+        System.out.println(petStoreService.text);
+    }
+
+    public static void start2(){
+        GenericApplicationContext context = new GenericApplicationContext();
+        new XmlBeanDefinitionReader(context).loadBeanDefinitions("Spring/services.xml");
+        context.refresh();
+        PetStoreServiceImpl petStoreService = (PetStoreServiceImpl) context.getBean("petStore");
+
+        System.out.println(petStoreService.text);
+
+    }
 
 
     public static PetStoreServiceImpl createStatic(){
--- src/main/java/springFramework/core/PetStoreServiceImpl.java
+++ src/main/java/springFramework/core/PetStoreServiceImpl.java
@@ -1,5 +1,29 @@
 package springFramework.core;
 
 public class PetStoreServiceImpl {
+    int number;
+    String text;
+
+
+    public PetStoreServiceImpl(){
+        number = 123;
+        text = "jada chlopcy jada";
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 
 }
