8e319bff4dded4ded5ca26eb2c21ba56cf20530e
jplotnikieicz <https://dev.codedose.com/gitlab/>
2018-03-01 16:36:55 +0100

    Spring lerning

--- src/main/java/springFramework/core/Core.java
+++ src/main/java/springFramework/core/Core.java
@@ -11,12 +11,18 @@ public class Core {
     static int counter = 1;
 
     public static void main (String [] args){
-        start6();
+        start7();
 
     }
 
+    public static void start7(){
+        ApplicationContext context = new ClassPathXmlApplicationContext("Spring/Core/beans.xml");
+        SmapleClass2 sample = context.getBean("one",SmapleClass2.class);
+        System.out.println(sample.getText());
+    }
+
     public static void start6(){
-        ApplicationContext context = new ClassPathXmlApplicationContext("Spring/services.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("Spring/Core/services.xml");
         SmapleClass2 sample = context.getBean("sample22",SmapleClass2.class);
         System.out.println(sample.getText());
         System.out.println(sample.sampleClass.getText());
@@ -24,7 +30,7 @@ public class Core {
 
 
     public static void start5(){
-        ApplicationContext context = new ClassPathXmlApplicationContext("Spring/services.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("Spring/Core/services.xml");
         SmapleClass2 sample = context.getBean("sample2",SmapleClass2.class);
         System.out.println(sample.getText());
         System.out.println(sample.sampleClass.getText());
@@ -33,27 +39,27 @@ public class Core {
 
 
     public static void start4(){
-        ApplicationContext context = new ClassPathXmlApplicationContext("Spring/services.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("Spring/Core/services.xml");
         SampleClass sample = context.getBean("noStaticSample",SampleClass.class);
         System.out.println(sample.getText());
     }
 
     public static void start3(){
-        ApplicationContext context = new ClassPathXmlApplicationContext("Spring/services.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("Spring/Core/services.xml");
         SampleClass sample = context.getBean("sample",SampleClass.class);
         System.out.println(sample.getText());
     }
 
     public static void start2(){
         GenericApplicationContext context = new GenericApplicationContext();
-        new XmlBeanDefinitionReader(context).loadBeanDefinitions("Spring/services.xml");
+        new XmlBeanDefinitionReader(context).loadBeanDefinitions("Spring/Core/services.xml");
         context.refresh();
         PetStoreServiceImpl petStoreService = (PetStoreServiceImpl) context.getBean("petStore");
         System.out.println(petStoreService.getText());
     }
 
     public static void start1(){
-        ApplicationContext context = new ClassPathXmlApplicationContext("Spring/services.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("Spring/Core/services.xml");
         PetStoreServiceImpl petStoreService = context.getBean("pet",PetStoreServiceImpl.class);
         System.out.println(petStoreService.getText());
     }
